6c928cb8a1337adb72f46122085b6b32f58086f2
autodocs <autodocs>
2016-11-12 05:47:44 +0000

    build based on 0077fab

--- latest/search_index.js
+++ latest/search_index.js
@@ -85,7 +85,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "Add-ons",
     "category": "section",
-    "text": "Because DifferentialEquations.jl has a common interface on the solutions, it is easy to add functionality to the entire DiffEq ecosystem by developing it to the solution interface. These pages describe the add-on analysis tools which are available.Pages = [\n    \"addons/function_definition_macros.md\",\n    \"addons/parameter_estimation.md\",\n    \"addons/sensitivity.md\",\n    \"addons/dev_and_test.md\"\n]\nDepth = 2"
+    "text": "Because DifferentialEquations.jl has a common interface on the solutions, it is easy to add functionality to the entire DiffEq ecosystem by developing it to the solution interface. These pages describe the add-on analysis tools which are available.Pages = [\n    \"addons/parameterized_functions.md\",\n    \"addons/parameter_estimation.md\",\n    \"addons/sensitivity.md\",\n    \"addons/dev_and_test.md\"\n]\nDepth = 2"
 },
 
 {
@@ -301,7 +301,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Common Solver Options",
     "title": "Common Solver Options",
     "category": "section",
-    "text": "The DifferentialEquations.jl universe has a large set of common arguments available for the solve function. These arguments apply to solve on any problem type and are only limited by limitations of the specific implementations.Many of the defaults depend on the algorithm or the package the algorithm derives from. For more detailed information on the defaults and the available options for specific algorithms / packages, see the Option Availability manual page."
+    "text": "The DifferentialEquations.jl universe has a large set of common arguments available for the solve function. These arguments apply to solve on any problem type and are only limited by limitations of the specific implementations.Many of the defaults depend on the algorithm or the package the algorithm derives from. Not all of the interface is provided by every algorithm. For more detailed information on the defaults and the available options for specific algorithms / packages, see the manual pages for the solvers of specific problems."
 },
 
 {
@@ -1689,19 +1689,51 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "addons/function_definition_macros.html#",
-    "page": "Function Definition Macros",
-    "title": "Function Definition Macros",
+    "location": "addons/parameterized_functions.html#",
+    "page": "ParameterizedFunctions",
+    "title": "ParameterizedFunctions",
     "category": "page",
     "text": ""
 },
 
 {
-    "location": "addons/function_definition_macros.html#Function-Definition-Macros-1",
-    "page": "Function Definition Macros",
-    "title": "Function Definition Macros",
+    "location": "addons/parameterized_functions.html#ParameterizedFunctions-1",
+    "page": "ParameterizedFunctions",
+    "title": "ParameterizedFunctions",
     "category": "section",
-    "text": "DifferentialEquations.jl provides a set of macros for more easily and legibly defining your differential equations. It exploits the standard notation for mathematically writing differential equations and the notation for \"punching differential equations into the computer\"; effectively doing the translation step for you. This is best shown by an example. Say we want to solve the ROBER model. Using the @ode_define macro, we can do this by writing:f = @ode_define begin\n  dy₁ = -k₁*y₁+k₃*y₂*y₃\n  dy₂ =  k₁*y₁-k₂*y₂^2-k₃*y₂*y₃\n  dy₃ =  k₂*y₂^2\nend k₁=>0.04 k₂=>3e7 k₃=>1e4This looks just like psudocode! The macro will expand this to the \"standard form\", i.e. the ugly computer form:f = (t,u,du) -> begin\n  du[1] = -0.04*u[1] + 1e4*u[2]*u[3]\n  du[2] = 0.04*u[1] - 3e7*u[2]^2 - 1e4*u[2]*u[3]\n  du[3] = 3e7*u[2]^2\nendNote that one doesn't need to use numbered variables: DifferentialEquations.jl will number the variables for you. For example, the follows defines the function for the Lotka-Voltera model:f = @ode_define begin\n  dx = a*x - b*x*y\n  dy = -c*y + d*x*y\nend a=1.5 b=1 c=3 d=1The other macro which is currently provided is the @fem_define macro. This macro is for parsing and writing FEM functions. For example, in the FEM methods you have to use x[:,1] instead of x and x[:,2] instead of y. The macro will automatically do this replacement, along with adding in parameters. Since FEM functions are more general, we also have to give it the function signature. Using the macro looks like this:f  = @fem_define (x) begin\n  sin(α.*x).*cos(α.*y)\nend α=>2π\ngD = @fem_define (x) begin\n  sin(α.*x).*cos(α.*y)/β\nend α=>2π β=>8π*πThis is equivalent to the definitionf(x) = sin(2π.*x[:,1]).*cos(2π.*x[:,2])\ngD(x) = sin(2π.*x[:,1]).*cos(2π.*x[:,2])/(8π*π)The true power comes in when dealing with nonlinear equations. The second argument, which we skipped over as (), is for listing the variables you wish to define the equation by. Mathematically you may be using u,v,w, etc., but for array-based algorithms you need to use u[:,1],u[:,2],etc. To avoid obfuscated code, the @fem_define macro does this conversion. For example:l = @fem_define (t,x,u) begin\n  du = ones(length(u))-α*u\n  dv = ones(length(v))-v\nend α=>0.5says there are two equations, one for u: (ones(length(u))-α*u) and one for v: (ones(length(v))-v). This expands to the equationl = (t,x,u)  -> [ones(size(x,1))-.5u[:,1]   ones(size(x,1))-u[:,2]]When you have 10+ variables, using @fem_define leads to code which is much easier to read!"
+    "text": ""
+},
+
+{
+    "location": "addons/parameterized_functions.html#Function-Definition-1",
+    "page": "ParameterizedFunctions",
+    "title": "Function Definition",
+    "category": "section",
+    "text": "DifferentialEquations.jl provides a set of macros for more easily and legibly defining your differential equations. It exploits the standard notation for mathematically writing differential equations and the notation for \"punching differential equations into the computer\"; effectively doing the translation step for you. This is best shown by an example. Say we want to solve the ROBER model. Using the @ode_def macro from ParameterizedFunctions.jl, we can do this by writing:using ParameterizedFunctions\nf = @ode_def ROBER begin\n  dy₁ = -k₁*y₁+k₃*y₂*y₃\n  dy₂ =  k₁*y₁-k₂*y₂^2-k₃*y₂*y₃\n  dy₃ =  k₂*y₂^2\nend k₁=>0.04 k₂=>3e7 k₃=>1e4This looks just like psudocode! The macro will expand this to the \"standard form\", i.e. the ugly computer form:f = (t,u,du) -> begin\n  du[1] = -0.04*u[1] + 1e4*u[2]*u[3]\n  du[2] = 0.04*u[1] - 3e7*u[2]^2 - 1e4*u[2]*u[3]\n  du[3] = 3e7*u[2]^2\nendNote that one doesn't need to use numbered variables: DifferentialEquations.jl will number the variables for you. For example, the follows defines the function for the Lotka-Volterra model:f = @ode_def LotkaVolterra begin\n  dx = a*x - b*x*y\n  dy = -c*y + d*x*y\nend a=>1.5 b=>1 c=>3 d=1"
+},
+
+{
+    "location": "addons/parameterized_functions.html#Extra-Features-1",
+    "page": "ParameterizedFunctions",
+    "title": "Extra Features",
+    "category": "section",
+    "text": "Functions defined using the @ode_def macro come with many other features. For example, since we used => for a, b, and c, these parameters are explicitly saved. That is, one can dof.a = 0.2to change the parameter f to 0.2. We can create a new function with new parameters using the name we gave the macro:g = LotkaVolterra(a=0.3,b=20.3)In this case, c will default to the value we gave it in the macro.Since the parameters are explicit, these functions can be used to analyze how the parameters effect the model. Thus ParameterizedFunctions, when coupled with the solvers, forms the backbone of functionality such as parameter estimation, parameter sensitivity analysis, and bifurcation analysis."
+},
+
+{
+    "location": "addons/parameterized_functions.html#Extra-Optimizations-1",
+    "page": "ParameterizedFunctions",
+    "title": "Extra Optimizations",
+    "category": "section",
+    "text": "Because the ParameterizedFunction holds the definition at a symbolic level, optimizations are provided by SymEngine. Using the symbolic calculator, in-place functions for many things such as Jacobians, Hessians, etc. are symbolically pre-computed. In addition, functions for the inverse Jacobian, Hessian, etc. are also pre-computed. In addition, parameter gradients and Jacobians are also used.Normally these will be computed fast enough that the user doesn't have to worry. However, in some cases you may want to restrict the number of functions (or get rid of a warning). Macros like @ode_def_nohes turn off the Hessian calculations, and @ode_def_noinvjac turns of the Jacobian inversion. For more information, please see the ParameterizedFunctions.jl documentation."
+},
+
+{
+    "location": "addons/parameterized_functions.html#Finite-Element-Method-Macros-1",
+    "page": "ParameterizedFunctions",
+    "title": "Finite Element Method Macros",
+    "category": "section",
+    "text": "The other macro which is currently provided is the @fem_def macro. This macro is for parsing and writing FEM functions. For example, in the FEM methods you have to use x[:,1] instead of x and x[:,2] instead of y. The macro will automatically do this replacement, along with adding in parameters. Since FEM functions are more general, we also have to give it the function signature. Using the macro looks like this:f  = @fem_def (x) begin\n  sin(α.*x).*cos(α.*y)\nend α=>2π\ngD = @fem_def (x) begin\n  sin(α.*x).*cos(α.*y)/β\nend α=>2π β=>8π*πThis is equivalent to the definitionf(x) = sin(2π.*x[:,1]).*cos(2π.*x[:,2])\ngD(x) = sin(2π.*x[:,1]).*cos(2π.*x[:,2])/(8π*π)The true power comes in when dealing with nonlinear equations. The second argument, which we skipped over as (), is for listing the variables you wish to define the equation by. Mathematically you may be using u,v,w, etc., but for array-based algorithms you need to use u[:,1],u[:,2],etc. To avoid obfuscated code, the @fem_def macro does this conversion. For example:l = @fem_def (t,x,u) begin\n  du = ones(length(u))-α*u\n  dv = ones(length(v))-v\nend α=>0.5says there are two equations, one for u: (ones(length(u))-α*u) and one for v: (ones(length(v))-v). This expands to the equationl = (t,x,u)  -> [ones(size(x,1))-.5u[:,1]   ones(size(x,1))-u[:,2]]When you have 10+ variables, using @fem_def leads to code which is much easier to read!"
 },
 
 {
