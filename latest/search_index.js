6396f1c6c8f7f9ae5ec32deca94c2c67917e88e6
autodocs <autodocs>
2017-11-24 20:11:11 +0000

    build based on 4b38c5e

--- latest/search_index.js
+++ latest/search_index.js
@@ -4261,7 +4261,15 @@ var documenterSearchIndex = {"docs": [
     "page": "Physical Models",
     "title": "N-Body Problems",
     "category": "section",
-    "text": ""
+    "text": "nprob = NBodyProblem(f, mass, pos, vel, tspan)where f is the potential function, mass is the mass matrix, pos and vel are ArrayPartitions for the intial positions and velocity, and tspan is the timespan to solve on."
+},
+
+{
+    "location": "models/physical.html#Example-1",
+    "page": "Physical Models",
+    "title": "Example",
+    "category": "section",
+    "text": "In this example we will model the outer solar system planets.G = 2.95912208286e-4\nM = [1.00000597682, 0.000954786104043, 0.000285583733151, 0.0000437273164546, 0.0000517759138449, 1/1.3e8]\ninvM = inv.(M)\nplanets = [\"Sun\", \"Jupiter\", \"Saturn\", \"Uranus\", \"Neptune\", \"Pluto\"]\n\npos_x = [0.0,-3.5023653,9.0755314,8.3101420,11.4707666,-15.5387357]\npos_y = [0.0,-3.8169847,-3.0458353,-16.2901086,-25.7294829,-25.2225594]\npos_z = [0.0,-1.5507963,-1.6483708,-7.2521278,-10.8169456,-3.1902382]\npos = ArrayPartition(pos_x,pos_y,pos_z)\n\nvel_x = [0.0,0.00565429,0.00168318,0.00354178,0.00288930,0.00276725]\nvel_y = [0.0,-0.00412490,0.00483525,0.00137102,0.00114527,-0.00170702]\nvel_z = [0.0,-0.00190589,0.00192462,0.00055029,0.00039677,-0.00136504]\nvel = ArrayPartition(vel_x,vel_y,vel_z)\n\ntspan = (0.,200_000)\n\nconst ∑ = sum\nconst N = 6\npotential(t, x, y, z, M) = -G*∑(i->∑(j->(M[i]*M[j])/sqrt((x[i]-x[j])^2 + (y[i]-y[j])^2 + (z[i]-z[j])^2), 1:i-1), 2:N)\nnprob = NBodyProblem(potential, M, pos, vel, tspan)\nsol = solve(nprob,Yoshida6(), dt=100)"
 },
 
 {
