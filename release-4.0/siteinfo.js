d629a4aeba2439898a070129399f5cc5bd8c999b
autodocs <autodocs>
2018-02-03 08:15:23 +0000

    build based on f09359b

---
+++ release-4.0/siteinfo.js
@@ -0,0 +1,1 @@
+var DOCUMENTER_CURRENT_VERSION = "release-4.0";
--- versions.js
+++ versions.js
@@ -1,6 +1,7 @@
 var DOC_VERSIONS = [
   "stable",
   "latest",
+  "release-4.0",
   "release-3.2",
   "release-3.0",
   "release-2.1",
@@ -9,6 +10,7 @@ var DOC_VERSIONS = [
   "release-1.8",
   "release-1.6",
   "release-1.10",
+  "v4.0.1",
   "v3.2.0",
   "v3.0.0",
   "v2.1.0",
