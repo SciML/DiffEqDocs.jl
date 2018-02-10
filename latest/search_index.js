c775f4bb4e31beb61fdf387d8b6f8b08d8f00630
autodocs <autodocs>
2018-02-10 14:01:46 +0000

    build based on 80ac6c1

--- latest/search_index.js
+++ latest/search_index.js
@@ -1221,7 +1221,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Discrete Problems",
     "title": "Mathematical Specification of a Discrete Problem",
     "category": "section",
-    "text": "To define an Discrete Problem, you simply need to give the function f and the initial condition u which define a function map:u_n+1 = f(upt_n)f should be specified as f(u,p,t) (or in-place as f(du,u,p,t)), and u₀ should be an AbstractArray (or number) whose geometry matches the desired geometry of u. Note that we are not limited to numbers or vectors for u₀; one is allowed to provide u₀ as arbitrary matrices / higher dimension tensors as well.Note that if the discrete solver is set to have scale_by_time=true, then the problem is interpreted as the map:u_n+1 = u_n + dtf(upt_n)"
+    "text": "To define an Discrete Problem, you simply need to give the function f and the initial condition u which define a function map:u_n+1 = f(upt_n+1)f should be specified as f(u,p,t) (or in-place as f(du,u,p,t)), and u₀ should be an AbstractArray (or number) whose geometry matches the desired geometry of u. Note that we are not limited to numbers or vectors for u₀; one is allowed to provide u₀ as arbitrary matrices / higher dimension tensors as well. t_{n+1} is the current time at which the map is applied. For a FunctionMap with defaults,  t_n = t0 + n*dt (with dt=1 being the default). For continuous-time Markov chains this is the time at which the change is occuring.Note that if the discrete solver is set to have scale_by_time=true, then the problem is interpreted as the map:u_n+1 = u_n + dtf(upt_n)"
 },
 
 {
