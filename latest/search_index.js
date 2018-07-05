733c3ee9d6bf35b8a170c29ddc62be425e03f79c
autodocs <autodocs>
2018-07-05 01:28:25 +0000

    build based on 6d5a467

--- latest/search_index.js
+++ latest/search_index.js
@@ -2181,7 +2181,7 @@ var documenterSearchIndex = {"docs": [
     "page": "ODE Solvers",
     "title": "Exponential Propagation Iterative Runge-Kutta Methods (EPIRK)",
     "category": "section",
-    "text": "Exp4 - 4th order EPIRK scheme with stiff order 4. Fixed time stepping only."
+    "text": "Exp4 - 4th order EPIRK scheme. Fixed time stepping only.\nEPIRK4s3A - 4th order EPIRK scheme with stiff order 4. Fixed time stepping only.\nEPIRK4s3B - 4th order EPIRK scheme with stiff order 4. Fixed time stepping only."
 },
 
 {
