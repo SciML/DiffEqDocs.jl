f352df3b8e53d21860267e8a40b7871dc6f824f6
autodocs <autodocs>
2018-11-28 14:41:30 +0000

    build based on c50fc76

--- latest/search_index.js
+++ latest/search_index.js
@@ -161,14 +161,6 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "tutorials/ode_example.html#Note-1",
-    "page": "Ordinary Differential Equations",
-    "title": "Note",
-    "category": "section",
-    "text": "If that code errors, it is most likely due to versioning issues.Pkg.status(\"DifferentialEquations\")should return a version higher than v4.0.0. If not, Pkg.update() to get the latest versions. If that does not work, then there is some other package upper bounding the allowed version of DifferentialEqautions.jl. The changes are described at length here:http://juliadiffeq.org/2018/01/24/Parameters.htmlIf you don\'t wish to update right now, simply doPkg.pin(\"DifferentialEquations\",v\"3.1.0\")and use the sidebar to change the documentation to v3.2.0."
-},
-
-{
     "location": "tutorials/ode_example.html#Step-1:-Defining-a-Problem-1",
     "page": "Ordinary Differential Equations",
     "title": "Step 1: Defining a Problem",
