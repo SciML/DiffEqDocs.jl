116c950d6bc6d9a42349e3296f904b9a202e77e0
autodocs <autodocs>
2019-02-11 14:26:00 +0000

    build based on a41ff4e

--- latest/search_index.js
+++ latest/search_index.js
@@ -4369,6 +4369,14 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
+    "location": "analysis/parameter_estimation.html#Using-JuMP-with-DiffEqParamEstim-1",
+    "page": "Parameter Estimation and Bayesian Analysis",
+    "title": "Using JuMP with DiffEqParamEstim",
+    "category": "section",
+    "text": "JuMP is a domain-specific modeling language  for mathematical optimization embedded in Julia.using OrdinaryDiffEq, ParameterizedFunctions, DiffEqParamEstim\nusing JuMP, NLopt, PlotsLet\'s define the Lorenz equation to use as our exampleg = @ode_def begin\n  dx = σ*(y-x)\n  dy = x*(ρ-z) - y\n  dz = x*y - β*z\nend σ ρ βLet\'s get a solution of the system with parameter values σ=10.0 ρ=28.0 β=8/3 to use as our data. We define some convenience functions model_ode (to create an ODEProblem) and solve_model(to obtain  solution of the ODEProblem) to use in a custom objective function later.u0 = [1.0;0.0;0.0]\nt = 0.0:0.01:1.0\ntspan = (0.0,1.0)\nmodel_ode(p_) = ODEProblem(g, u0, tspan,p_) \nsolve_model(mp_) = OrdinaryDiffEq.solve(model_ode(mp_), Tsit5(),saveat=0.01)\nmock_data = Array(solve_model([10.0,28.0,8/3]))Now we define a custom objective function to pass for optimization to JuMP using the build_loss_objective described above provided by DiffEqParamEstim that defines an objective  function for the parameter estimation problem.loss_objective(mp_, dat) = build_loss_objective(model_ode(mp_), Tsit5(), L2Loss(t,dat))We create a JuMP model, variables, set the obbjective function and the choice of  oprimization algorithm to be used in the JuMP syntax. You can read more about this in  JuMP\'s documentation.juobj(args...) = loss_objective(args, mock_data)(args)\njumodel = Model()\nJuMP.register(jumodel, :juobj, 3, juobj, autodiff=true)\n@variables jumodel begin\n    σ,(start=8)\n    ρ,(start=25.0)\n    β,(start=10/3)\nend\n@NLobjective(jumodel, Min, juobj(σ, ρ, β))\nsetsolver(jumodel, NLoptSolver(algorithm=:LD_MMA))Let\'s call the optimizer to obtain the fitted parameter values.sol = JuMP.solve(jumodel)\nbest_mp = getvalue.(getindex.((jumodel,), Symbol.(jumodel.colNames)))Let\'s compare the solution at the obtained parameter values and our data. sol = OrdinaryDiffEq.solve(best_mp |> model_ode, Tsit5())\nplot(getindex.(sol.(t),1))\nscatter!(mock_data, markersize=2)(Image: jumpestimationplot)"
+},
+
+{
     "location": "analysis/parameter_estimation.html#Generalized-Likelihood-Example-1",
     "page": "Parameter Estimation and Bayesian Analysis",
     "title": "Generalized Likelihood Example",
