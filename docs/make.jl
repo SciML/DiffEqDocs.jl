85844813d040b6c6707dbecd1967dd4da269d0f2
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2020-07-19 13:30:39 -0400

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -16,7 +16,7 @@ makedocs(modules=[DiffEqBase,DiffEqProblemLibrary,DiffEqBiological,ODEProblemLib
          doctest=false, clean=true,
          format = Documenter.HTML(analytics = "UA-90474609-3",
                                   assets = ["assets/favicon.ico"],
-                                  canonical="https://docs.sciml.ai/stable/"),
+                                  canonical="https://diffeq.sciml.ai/stable/"),
          sitename="DifferentialEquations.jl",
          authors="Chris Rackauckas",
          pages = Any[
