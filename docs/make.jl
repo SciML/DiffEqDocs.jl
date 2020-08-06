dc23f53957282f99ff419828561c6eb5fdbc91f9
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2020-08-06 02:17:44 -0400

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,4 +1,4 @@
-using Documenter,DiffEqBase,DiffEqProblemLibrary,DiffEqBiological
+using Documenter, DiffEqBase, DiffEqProblemLibrary, Catalyst
 
 ODEProblemLibrary = DiffEqProblemLibrary.ODEProblemLibrary
 ODEProblemLibrary.importodeproblems()
@@ -12,7 +12,7 @@ DDEProblemLibrary.importddeproblems()
 DAEProblemLibrary = DiffEqProblemLibrary.DAEProblemLibrary
 DAEProblemLibrary.importdaeproblems()
 
-makedocs(modules=[DiffEqBase,DiffEqProblemLibrary,DiffEqBiological,ODEProblemLibrary,SDEProblemLibrary,DDEProblemLibrary,DAEProblemLibrary],
+makedocs(modules=[DiffEqBase,DiffEqProblemLibrary,Catalyst,ODEProblemLibrary,SDEProblemLibrary,DDEProblemLibrary,DAEProblemLibrary],
          doctest=false, clean=true,
          format = Documenter.HTML(analytics = "UA-90474609-3",
                                   assets = ["assets/favicon.ico"],
