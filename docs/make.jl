e83b79f061832ba69f7b37139638a079701a418b
ArnoStrouwen <arno.strouwen@telenet.be>
2022-11-10 17:28:59 +0100

    reproducible docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,9 @@
 using Documenter, DiffEqBase, SciMLBase, OrdinaryDiffEq
 import ODEProblemLibrary, SDEProblemLibrary, DDEProblemLibrary, DAEProblemLibrary
 
+cp("./docs/Manifest.toml", "./docs/src/assets/Manifest.toml", force = true)
+cp("./docs/Project.toml", "./docs/src/assets/Project.toml", force = true)
+
 include("pages.jl")
 
 makedocs(modules = [
